622f5d50487fb71603aca8043df2739754980b13
nomadicux <42170173+nomadicux@users.noreply.github.com>
2019-01-16 20:54:27 +1100

    more styling
    Completed more styling. Particularly on the desktop version. Little more to go though. Need to fix javascript for scroll to top button.

--- Site/js/javascript.js
+++ Site/js/javascript.js
@@ -1,3 +1,4 @@
+// Code for Testimonial slider section
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,14 +14,37 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+// End of code for Testimonial slider section
+
+// Scroll to top code
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// End scroll to top code

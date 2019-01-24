d0dbf424ec3504a7653884d76863d0b5487ffaaf
nomadicux <42170173+nomadicux@users.noreply.github.com>
2019-01-24 14:29:27 +1100

    added phpmailer
    Added php mailer and configured so that it sends all the right details.

--- Site/php/mailer.php
+++ Site/php/mailer.php
@@ -1,19 +1,40 @@
 <?php
-	if(isset($_POST['submit']))
-	{
-		$to = "jhiles@nomadicux.com";
-		$subject = "Customer Enquiry";
-		$name_field = $_POST['userName'];
-		$email_field = $_POST['userMail'];
-		$message = $_POST['userMessage'];
 
-		$body = "From: $name_field\n E-Mail: $email_field\n Message:\n $message";
+$name = $_POST['userName'];
+$email = $_POST['userEmail'];
+$message = $_POST['userMessage'];
 
-		echo "Thanks for your enquiry! Expect a response within 24 hours.";
-		mail($to, $subject, $body);
-	}
-	else
-	{
-		echo "ERROR: Email not sent. Please go back and try again or manually send to jhiles@nomadicux.com";
-	}
+//$myEmail = 'jhiles@nomadicux.com';
+
+require '../PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.office365.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'jhiles@nomadicux.com';                 // SMTP username
+$mail->Password = '!B3lind41993';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->From = 'jhiles@nomadicux.com';
+$mail->addAddress('jhiles@nomadicux.com');     // Add a recipient
+
+$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Customer Enquiry From: ' . $name;
+$mail->Body    = $message . '</br>' . 'From: ' . $email;
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 ?>
